1a35cc75b96fc778a7678854bcec19484d394c1f
Rubaakter <ruba.akter@student.kyh.se>
2021-10-05 12:45:38 +0200

    Json file is used to solve iteration 4.

---
+++ Iteration_4.py
@@ -0,0 +1,42 @@
+import json
+QUESTIONS = "questions.json"
+
+
+def load_question():
+    with open(QUESTIONS) as q:
+        return json.load(q)
+
+
+def main():
+    QUESTIONS = load_question()
+    correct_answers = 0
+    for i in QUESTIONS:
+        print(i)
+        answer = (input("Ditt svar (från 1/2/3): "))
+        print(answer)
+        if answer == QUESTIONS[i]:
+            print("Rätt svar!")
+            correct_answers += 1
+        else:
+            print("Fel svar")
+            print(f"Rätt svar: {QUESTIONS[i]} ")
+
+    print(f"""                                """)
+    print(f"""**** RESULTAT ****""")
+    print(f"Du fick {str(correct_answers)} poäng av {str(len(QUESTIONS))}  möjliga.")
+
+
+if __name__ == '__main__':
+    main()
+
+
+# def load_questions():
+#     try:
+#         with open(QUESTIONS) as q:
+#             return json.load(q)
+#     except FileNotFoundError:
+#         with open(QUESTIONS, "w") as q:
+#             return
+# def save_questions(questions):
+#     with open(QUESTIONS, "r") as f:
+#         json.dump(questions, f)
--- Questions.py
+++ Questions.py
@@ -8,3 +8,46 @@ frågor = {"1. Vilken symbol används för att starta en kommentar? \n1. # \n2. 
              "8. Hur kan du kontrollera objektets typ? \n1. type() \n2. index()  \n3. typ()":"1",
              "9. Vad heter behållare för lagring av datavärden? \n1. lagring \n2. minne  \n3. variabel": "3",
              "10. Hur skriver man datatypen för ett helt tal? \n1. float \n2. int  \n3. str": "2"}
+
+# f1 ="""Vilken symbol används för att starta en kommentar?
+#     1. #
+#     2. &
+#     3. %"""
+# f2 ="""Vad är nyckelordet för att definiera en funktion i python?
+#     1. define
+#     2. Def
+#     3. def"""
+# f3 = """Vilken funktion används för att skriva ut saker på skärmen?
+#     1. print
+#     2. skriva
+#     3. P"""
+# f4 = """Hur tar man fram längden på listan i variabeln 'fruits'?
+#      1. length
+#      2. Leng
+#      3. len"""
+# f5 = """ Vad heter nyckelordet för att göra en loop i Python?
+#     1. loop
+#     2. for
+#     3. if"""
+# f6 = """Hur får man tillgång till ett enskilt objekt på listan?
+#      1. len
+#      2. key
+#      3. index"""
+# f7 = """Vilken operatör returnerar heltalets återstod (modulo) av divisionen?
+#      1. %
+#      2. /
+#      3. //"""
+# f8 = """Hur kan du kontrollera objektets typ?
+#     1. index()
+#     2. type()
+#     3. typ()"""
+# f9 = """Vad heter behållare för lagring av datavärden?
+#      1. variabel
+#      2. lagring
+#      3. minne"""
+# f10 = """Hur skriver man datatypen för ett helt tal?
+#     1. float
+#     2. int
+#     3. str"""
+# svar = {f1: "1", f2: "3", f3: "1", f4:"3", f5:"2", f6: "3", f7: "1", f8: "2", f9: "1", f10: "2"}
+# print("Du fick " + str(correct_answers) + " poäng av " + str(len(frågor)) + " möjliga.")
