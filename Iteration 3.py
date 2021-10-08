4b733c94d10b7aaeca871bf1ddad18f6755c8dc4
Rubaakter <ruba.akter@student.kyh.se>
2021-10-08 14:27:08 +0200

    All questions have made.

--- Iteration 3.py
+++ Iteration 3.py
@@ -1,23 +1,35 @@
-from Questions import frågor
+import json
+QUESTIONS = "questions.json"
+
+
+def load_question():
+    try:
+        with open(QUESTIONS, encoding='utf-8') as question:
+            return json.load(question)
+    except FileNotFoundError:
+        with open(QUESTIONS, "w") as question:
+            return
 
 
 def main():
     print("EXEMPELKÖRNING:    ")
     print("'''                      ")
+    QUESTIONS = load_question()
     correct_answers = 0
-    for i in frågor:
+    for i in QUESTIONS:
         print(i)
         answer = (input("Ditt svar (från 1/2/3): "))
         print(answer)
-        if answer == frågor[i] :
+        if answer == QUESTIONS[i]:
             print("Rätt svar!")
             correct_answers += 1
         else:
             print("Fel svar")
-            print(f"Rätt svar: {frågor[i]} ")
-    print("                                      ")
+            print(f"Rätt svar: {QUESTIONS[i]} ")
+        print(" "*80)
+    print(f"""                                """)
     print(f"""**** RESULTAT ****""")
-    print(f"Du fick {str(correct_answers)} poäng av {str(len(frågor))}  möjliga.")
+    print(f"Du fick {str(correct_answers)} poäng av {str(len(QUESTIONS))}  möjliga.")
 
 
 if __name__ == '__main__':
